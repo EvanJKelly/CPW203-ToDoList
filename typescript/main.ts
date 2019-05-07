b5ddf9cdedbc64749c2bfe7f3f225be46394862c
EVAN Kelly <kelly7682@students.cptc.edu>
2019-05-07 14:42:38 -0700

    Created prototype ToDo Item

--- typescript/main.ts
+++ typescript/main.ts
@@ -29,10 +29,25 @@ if(testItem.isComplete){
     //Save ToDo object
 
 window.onload = function(){
+   
     let addBtn = <HTMLLinkElement> 
         document.querySelector("#create-item > button");
     addBtn.click = processNewItem;
 
+    let readItemBtn = <HTMLElement>
+        document.querySelector("#read-item > button");
+    readItemBtn.onclick = readItem;
+
+}
+
+const itemKey:string = "todo";
+
+function readItem(){
+    //get item from storage 
+    let item:ToDoItem = JSON.parse(localStorage.getItem(itemKey));
+
+    alert(item.title);
+    alert(item.description);
 }
 
 function processNewItem(){
@@ -42,6 +57,42 @@ function processNewItem(){
     clearForm();
 }
 
+function clearForm(){
+    //We could alternatively, wrap all inputs in
+    //a <form> and reset the form
+
+    //clear all textboxes and textarea
+    let textElements = 
+        document.querySelectorAll("input[type=text], textarea");
+    for(let i = 0; i < textElements.length; i++){
+        (<HTMLInputElement>textElements[i]).value = "";
+    }
+    //uncheck is complete
+    let isCompleteBox = <HTMLInputElement>
+        document.querySelector("#is-complete");
+    isCompleteBox.checked = false;
+    //reset select list
+    let urgencyList = <HTMLSelectElement>
+    document.querySelector("#urgency");
+    urgencyList.selectedIndex = 0;
+}
+
+
+function notifyUser(){
+    alert("Your item was saved");
+}
+function saveItem(item:ToDoItem): void{
+
+    let data:string = JSON.stringify(item);
+    console.log("Converting todoitem into JSON string...");
+    console.log(data);
+
+
+    //ensure user can use localStorage
+    if(typeof(Storage) != "undefined"){
+        localStorage.setItem("todo", data);
+    }
+}
 /**
  * Get all user input from Form 
  * and wrap it in a ToDoItem
@@ -64,7 +115,8 @@ function getItemFromForm():ToDoItem{
 
     item.isComplete = (<HTMLInputElement>document.getElementById("is-complete")).checked;
 
-    item.urgency = document.getElementById("")
+    let urgencyElem = <HTMLSelectElement>document.getElementById("urgency");
+    item.urgency = urgencyElem.options[urgencyElem.selectedIndex].text;
 
     return item;
 }
